055904aee1b3d27711b6f26b82b45d093791fd44
birusod <birusod@gmail.com>
2024-01-03 21:46:29 -0800

    RProgr setup - 1

---
+++ RDocs/dsr/r-template.R
@@ -0,0 +1,60 @@
+
+# Load packages -----------------------------------------------------------
+
+pacman::p_load(
+  tidytuesdayR, tidyverse, janitor, scales, ggthemes,
+  showtext, patchwork, ggtext, glue)
+
+
+# Loading data -------------------------------------------------------------
+
+tuesdata <- tidytuesdayR::tt_load(date_chr)
+
+
+# Loading fonts ------------------------------------------------------------
+
+font_add_google("Roboto", "roboto")
+font_add_google("Fira code", "firac")
+font_add_google("Fira sans", "firas")
+showtext_auto()
+
+
+# Defining colors ---------------------------------------------------------
+
+bg_col <- ""
+text_col <- ""
+highlight_col <- ""
+
+
+# Wrangling Data ----------------------------------------------------------
+
+
+
+# Define texts and annotations --------------------------------------------
+
+# social <- nrBrand::social_caption(
+#   bg_colour = bg_col,
+#   icon_colour = highlight_col,
+#   font_colour = text_col,
+#   font_family = "roboto"
+# )
+title <- ""
+st <- ""
+cap <- paste0(
+  "**Data**: <br>", #social
+)
+
+
+# Data Viz -------------------------------------------------------------------
+
+
+
+# Saving Plots and Gifs ------------------------------------------------------
+
+# save the final plot as 'final_plot'
+ggsave(
+  filename = file.path(
+    wkf,
+    pfolder, 
+    paste0("final_plot", ".png"))
+  )
---
+++ RDocs/dsr/temp_tt1.R
@@ -0,0 +1,121 @@
+# creating a template for tidytuesday script
+# inspired by Nicola Rennie's blog post 
+# https://nrennie.rbind.io/blog/script-templates-r/
+
+
+# only need to update these 5 parameters, then: Alt+Cmd+r (Cmd+A+Enter)
+# ****************************************************************
+week_date <- '2023-11-14'
+week_number <- 'W46'
+weekly_data <- 'DiwaliSales'
+current_week_folder <- paste0(week_number, '_', weekly_data)
+week_title <- 'Diwali Sales'
+plot_folder <- 'plots_w46'
+final_plot <- 'final_plot_w46.png'
+#*****************************************************************
+
+use_tt_template <- function(
+    date_chr = week_date, 
+    week = current_week_folder, 
+    title = week_title, 
+    readme = TRUE) {
+  # check date in correct format
+  if (is.na(as.Date(date_chr, format = "%Y-%m-%d"))) {
+    stop("'date_chr' in incorrect format. Should be yyyy-mm-dd.")
+  }
+
+# Creating folders and files
+#title <- title #'Fair Use'
+#week <-  week #'w35_Fair Use'
+#date_chr <- date # "2023-08-29"
+yr <- sub("-.*", "", date_chr)
+date_strip <- stringr::str_remove_all(date_chr, "-")
+
+
+
+new_folder <- file.path(current_week_folder)
+
+# check if folder exist (to avoid overwriting)
+if (file.exists(new_folder)) {
+  stop("Please check that date and folder name are correct!")
+}
+
+if (!file.exists(new_folder)) {
+  dir.create(new_folder, recursive = TRUE)
+  message("Created new folder")
+}
+
+
+
+# create r file
+new_file <- file.path(new_folder, paste0(date_strip, ".R"))
+
+if (!file.exists(new_file)) {
+  file.create(new_file)
+  message("Created '.R' file")
+}
+
+# create a  new readme.md file
+new_readme <- file.path(new_folder, "README.md")
+if (!file.exists(new_readme)) {
+  file.create(new_readme)
+  message("Created 'README.md' file")
+}
+
+
+# create plots folder
+new_plot_folder <- file.path(current_week_folder, plot_folder)
+dir.create(new_plot_folder, recursive = TRUE)
+
+# Creating a template README file: readme-template.md 
+
+
+# copy lines to README file
+readme_txt <- readLines("readme-template.md")
+
+# replace placeholder text with variables
+readme_txt <- gsub(pattern = "title", replacement = title, x = readme_txt)
+#readme_txt <- gsub(pattern = "yr", replacement = yr, x = readme_txt)
+readme_txt <- gsub(pattern = "wkf", replacement = current_week_folder, x = readme_txt)
+readme_txt <- gsub(pattern = "pfolder", replacement = plot_folder, x = readme_txt)
+
+# write to file
+writeLines(readme_txt, con = new_readme)
+message("'README.md' contents copied")
+
+
+# Creating a template .R file r-template.R
+
+# Write to new r file: yyyymmdd.R
+# copy lines to .R file
+r_txt <- readLines("r-template.R")
+
+# replace placeholder text with variables
+#r_txt <- gsub(pattern = "yr", replacement = paste0("\"", yr, "\""), x = r_txt)
+r_txt <- gsub(
+  pattern = "date_chr", 
+  replacement = paste0("\"", date_chr, "\""), 
+  x = r_txt)
+r_txt <- gsub(
+  pattern = "wkf", 
+  replacement = paste0("\"", current_week_folder, "\""),
+  x = r_txt)
+r_txt <- gsub(
+  pattern = "pfolder",
+  replacement = paste0("\"", plot_folder, "\""),
+  x = r_txt)
+
+# write to new file
+writeLines(r_txt, con = new_file)
+message("'.R' contents copied")
+
+}
+
+
+# Excute function for new folder, file, readme:
+use_tt_template()
+
+
+
+
+
